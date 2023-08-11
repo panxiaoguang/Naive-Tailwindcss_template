d11ae157ea0b64f0c9f639cfd48c8d20824121d4
panxiaoguang <43256809+panxiaoguang@users.noreply.github.com>
2023-08-11 13:00:08 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { NButton } from 'naive-ui';
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <div class="flex flex-col w-24 mx-auto space-y-1">
+      <n-button>Default</n-button>
+      <n-button type="tertiary"> Tertiary </n-button>
+      <n-button type="primary"> Primary </n-button>
+      <n-button type="info"> Info </n-button>
+      <n-button type="success"> Success </n-button>
+      <n-button type="warning"> Warning </n-button>
+      <n-button type="error"> Error </n-button>
+    </div>
+  </div>
+</template>
